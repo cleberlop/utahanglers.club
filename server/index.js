8ad5c4b13e90db3fdd25e9cb77e13a7401d87be3
Cleber Albuquerque <cleber.lop@gmail.com>
2016-02-14 22:37:24 -0700

    es5 for connect-mongo

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var MongoStore = require('connect-mongo')(session);
+var MongoStore = require('connect-mongo/es5')(session);
 var config = require('./config.json');
 
 
